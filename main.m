1f10db1b3f0d85ef70a5219e4739d0f8a088f185
Kuba <kubalobo@gmail.com>
2017-05-17 17:29:03 +0200

    Super final

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-function [xyz, Xhat, wektor] = main(jono, tropo, radioSelected, zegar, aber)
+function [xyz, Xhat, wektor, sigmy] = main(jono, tropo, radioSelected, zegar, aber)
 
 c = 299792458;
 
@@ -10,7 +10,9 @@ wspPrzyblizone = [5009051.3990 -42072.4720 3935057.5040];
 odlRinex = [20279017.485; 25716551.950; 21875028.343; 23779959.609; 23259526.375; 21034103.473; 24265078.504; 20669965.650; 23270445.268; 22744417.506; 21962780.866];
 
 [wspolrzedneSatelitow, efe] = rinexLoad(t);
-wspolrzedneSatelitow
+%wspolrzedneSatelitow
+efe(1,1);
+efe(1,2);
 % =============== WSZYSTKIE POPRAWKI ZAPISYWANE SA W ODDZIELNYCH ZMIENNYCH ==================
 
 wspSatPop = zeros(11,3);
@@ -18,7 +20,7 @@ odlGeom = zeros(11,1);
 
 % POPRAWKA r�znicy czasu i uk�adu | odlGeom - odleglosc na podstawie przyblizen
 for i = 1:11
-    [wspSatPop(i,:), tem, odlGeom(i)] = poprawka_pozycja(wspolrzedneSatelitow(i,:), efe(1,i), wspPrzyblizone);
+    [wspSatPop(i,:), tem, odlGeom(i)] = poprawka_pozycja(wspolrzedneSatelitow(i,:), efe(i,1), wspPrzyblizone);
 end
 
 
@@ -33,7 +35,7 @@ for i = 1:11
     [A, E, z, phi, lambda] = elewacja(wspPrzyblizone, wspSatPop(i,:));
 
     if(jono)
-        klobWyn(i) = klobuchar(deg2rad(E/180), deg2rad(A), phi, lambda, efe(1,i)); % t zamiast efe ?
+        klobWyn(i) = klobuchar(deg2rad(E/180), deg2rad(A), phi, lambda, efe(i,1)); % t zamiast efe ?
     end
     elew(i) = E; % STOPNIE!
 end
@@ -74,11 +76,11 @@ end
 clockCor = zeros(11,1);
 timeAbber = zeros(11, 1);
 for i = 1:11
-    if(zegar)
-        clockCor(i) = ClockCorrections(efe(i, 2), efe(i, 3), efe(i, 4), efe(i, 1), t, timeAbber(i)); %tu nie wiem, czy wszystko jest ok
-    end
     if(aber)
         timeAbber(i) = EccentricityTimeAbberration(efe(i, 11)^2, efe(i, 9), efe(i, 21)); 
+    end
+    if(zegar)
+        clockCor(i) = ClockCorrections(efe(i, 2), efe(i, 3), efe(i, 4), t, tem, timeAbber(i)); %tu nie wiem, czy wszystko jest ok
     end
 end
 
@@ -101,6 +103,7 @@ for i = 1:11
     A(i,4) = 1;
 end
 
+
 if(radioSelected == 1)
     tropo = hop1;
 elseif(radioSelected == 2)
@@ -111,15 +114,26 @@ elseif(radioSelected == 4)
     tropo = saas2;
 end
 
-l = odlRinex - klobWyn - tropo + clockCor*c - timeAbber*c;
+l = odlRinex - klobWyn - tropo + clockCor*c + timeAbber*c;
 
 
 roznice = l - odlGeom;
-
+P=P(elew>10,elew>10);
+roznice=roznice(elew>10);
+A = A(elew>10,:);
 Xhat = (A'*P*A)\(A'*P*roznice);
+
 wektor = sqrt((Xhat(1))^2 + (Xhat(2))^2 + (Xhat(3))^2);
 
 
 xyz = wspPrawdziwe + Xhat(1:3)';
 
-% Jeszcze brakuje koncowego wyliczenia wspolrzednych i chyba tych dziwnych sigm
+ vhat =  roznice-A*Xhat;
+ varUnitWeight = (vhat'*P*vhat)./(11-4);
+ Cx = varUnitWeight * inv(A'*P*A);
+ sigX = sqrt(Cx(1,1));
+ 
+ sigY = sqrt(Cx(2,2));
+ sigZ = sqrt(Cx(3,3));
+
+ sigmy = [sigX sigY sigZ];
